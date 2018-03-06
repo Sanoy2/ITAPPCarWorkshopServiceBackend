5c61e6ac3d964a73f9177f963346efbcc6371495
Krzysztof_Tomkow <218514@student.pwr.edu.pl>
2018-03-06 22:35:23 +0100

    Imported models from database and create sample connection to DB in sample controller only GET method

---
+++ ITAPP_CarWorkshopService/Controllers/CarBrands2Controller.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ITAPP_CarWorkshopService.Controllers
+{
+    public class CarBrands2Controller : ApiController
+    {
+
+
+        // GET api/values/5
+        public List<Car_Brands> Get()
+        {
+            var db = new ITAPPCarWorkshopServiceDBEntities();
+            
+            var list = new List<Car_Brands>();
+
+            var carBrands = db.Car_Brands;
+
+            foreach (var carBrand in carBrands)
+            {
+                list.Add(carBrand);
+            }
+
+            return list;
+        }
+
+
+    }
+}
---
+++ ITAPP_CarWorkshopService/Controllers/CarBrandsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ITAPP_CarWorkshopService.Controllers
+{
+    public class CarBrandsController : ApiController
+    {
+
+
+        // GET api/values/5
+        public List<string> Get()
+        {
+            var db = new ITAPPCarWorkshopServiceDBEntities();
+            
+            List<string> list = new List<string>();
+
+            var carBrands = db.Car_Brands;
+
+            foreach (var carBrand in carBrands)
+            {
+                list.Add(carBrand.Brand_Name);
+            }
+
+            return list;
+        }
+
+
+    }
+}

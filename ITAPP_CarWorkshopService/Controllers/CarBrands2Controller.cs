92c9652a09c9231dd65b696ac5ec27ff8a0a96d4
PawelGolik <209319@student.pwr.edu.pl>
2018-03-07 14:57:45 +0100

    test 1.0

--- ITAPP_CarWorkshopService/Controllers/CarBrands2Controller.cs
+++ ITAPP_CarWorkshopService/Controllers/CarBrands2Controller.cs
@@ -10,12 +10,10 @@ namespace ITAPP_CarWorkshopService.Controllers
     public class CarBrands2Controller : ApiController
     {
 
-
         // GET api/values/5
         public Car_Brands Get(int id)
         {
             var db = new ITAPPCarWorkshopServiceDBEntities();
-
             return db.Car_Brands.FirstOrDefault((p) => p.Brand_ID == id);
         }
 

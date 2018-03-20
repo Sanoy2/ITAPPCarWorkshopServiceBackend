4ff38d9869615fccff8fbcdb8a04bb9dcb52509e
Sanoy2 <218514@student.pwr.edu.pl>
2018-03-20 13:09:12 +0100

    Created example protected method inside exampleAuthorizationController

---
+++ ITAPP_CarWorkshopService/Controllers/exampleAuthorizationController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ITAPP_CarWorkshopService.Controllers
+{
+    public class exampleAuthorizationController : ApiController
+    {
+        [HttpGet]
+        [Route("protected/exampleAuthorization")]
+        public string GetPrivate()
+        {
+            return "It works as private!";
+        }
+    }
+}

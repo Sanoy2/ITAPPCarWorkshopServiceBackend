cc132229ceac9c61817dd6985ad36a18d8950a0a
Krzysztof_Tomkow <218514@student.pwr.edu.pl>
2018-03-20 11:28:00 +0100

    Created TokenMessageHandler but it is not complete yet

---
+++ ITAPP_CarWorkshopService/Authorization/TokenMessageHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+
+namespace ITAPP_CarWorkshopService.Authorization
+{
+    public class TokenMessageHandler : DelegatingHandler
+    {
+        private const string AdminApiKey = "ITAPP";
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage httpRequestMessage, CancellationToken cancellationToken)
+        {
+            bool validKey = false;
+            IEnumerable<string> requestHeaders;
+            bool checkApiKeyExists = httpRequestMessage.Headers.TryGetValues("Token", out requestHeaders);
+            if(checkApiKeyExists)
+            {
+                if(requestHeaders.FirstOrDefault().Equals(AdminApiKey))
+                {
+                    validKey = true;
+                }
+            }
+        }
+    }
+}

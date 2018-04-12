344e716b704779555bb3cf9cd7541f4d5b27939b
Sanoy2 <krzysztof.tomkow@gmail.com>
2018-04-12 21:54:23 +0200

    Created CarBrandModel

---
+++ ITAPP_CarWorkshopService/DataModels/CarProfileModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ITAPP_CarWorkshopService.DataModels
+{
+    public class CarProfileModel
+    {
+        public int CarID { get; set; }
+        public int BrandID { get; set; }
+        public string Carmodel { get; set; }
+        public string CarVINNumber { get; set; }
+        public int CarProductionYear { get; set; }
+        public Nullable<int> CarFirstRegistrationYear { get; set; }
+    }
+}
